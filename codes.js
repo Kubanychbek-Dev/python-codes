4f5dc74e0907dfb0869520245b66f2004ec67176
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2024-11-01 23:08:04 +0300

    add codes.js

---
+++ codes.js
@@ -0,0 +1,16 @@
+export const codeLists = [
+  {
+    codeName: "Разбиение числа на цифры",
+    codeSelf: `
+           nums = 123
+      
+           c = nums % 10
+           nums = nums // 10
+           b = nums % 10
+           nums = nums // 10
+           a = nums % 10
+           
+           print(a, b, c)
+    `
+  }
+ ]
